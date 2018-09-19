408f7cf3fe98b6d1671e75757cfdc7431ea012a7
shohei-ojs <shohei.ojs@gmail.com>
2018-09-19 15:55:21 +0000

    add templates

--- chat/main.go
+++ chat/main.go
@@ -3,21 +3,29 @@ package main
 import (
 	"log"
 	"net/http"
+	"text/template"
+	"path/filepath"
+	"sync"
 )
 
-func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte(`
-			<html>
-				<head>
-					<title>チャット</title>
-				</head>
-				<body>
-					チャットしましょう！
-				</body>
-			</html>
-			`))
+type templateHandler struct {
+	once     sync.Once
+	filename string
+	templ    *template.Template
+}
+
+func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	t.once.Do(func() {
+		t.templ =
+				template.Must(template.ParseFiles(filepath.Join("templates",
+						t.filename)))
 	})
+	t.templ.Execute(w, nil)
+}
+
+func main() {
+	// root
+	http.Handle("/", &templateHandler{filename: "chat.html"})
 	// Webサーバーを開始します
 	if err := http.ListenAndServe(":80", nil); err != nil {
 		log.Fatal("ListenAndServe:", err)
